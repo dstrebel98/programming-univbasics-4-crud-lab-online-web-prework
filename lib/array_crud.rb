de064b70c1acec03c02695c849b93c8d4c3e852e
Erika N. Fairley <efairley98@gmail.com>
2019-07-08 17:14:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,25 +17,27 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  [red,]
+  [red,blue, yellow, orange]
+  .pop orange
 end
 
 def remove_element_from_start_of_array(array)
-  
+  [red, blue, yellow, orange]
+  .shift red
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  [1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+ [-1] 
 end
 
 def update_element_from_index(array, index_number, element)
-
+[colors, 1, silver]
 end
